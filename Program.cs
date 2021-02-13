772da522a687a017f41495a8f1c419b376d83b04
Victor Reipur <victor.reipur@gmail.com>
2021-02-13 20:34:29 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// C# How To Use The Override And Virtual Method.
+// https://www.youtube.com/watch?v=HmAqUHS05c8
+
+using System;
+
+namespace CSExampleOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Andrew", "Eberle");
+            System.Console.WriteLine(student.GetFullName);
+        }
+    }
+    class Student 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Student(string fn, string ln)
+        {
+            this.FirstName = fn;
+            this.LastName = ln;
+        }
+        public string GetFullName
+        {
+            get
+            {
+                return "Studen's Name: " + this.FirstName + " " + this.LastName;
+            }
+        }
+        class Teacher : Student
+        {
+            public Teacher(string fn, string ln) 
+            {
+                this.FirstName = fn;
+                this.LastName = ln;
+            }
+        }
+    }
+
+}
